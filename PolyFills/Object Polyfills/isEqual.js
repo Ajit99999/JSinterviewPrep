a91d0161a5e27d179c07daba30df20676154a300
Ajit99999 <sahoo.ajit1234@gmail.com>
2025-03-18 01:44:42 +0530

    isEqual polyfill

---
+++ PolyFills/Object Polyfills/isEqual.js
@@ -0,0 +1,71 @@
+function isEqual(arg1, arg2) {
+  if (typeof arg1 !== typeof arg2) {
+    return false;
+  }
+  if (typeof arg1 === "number" && typeof arg2 === "number") {
+    return arg1 === arg2;
+  }
+
+  if (typeof arg1 === "string" && typeof arg2 === "string") {
+    return arg1 === arg2;
+  }
+  if (typeof arg1 === "boolean" && typeof arg2 === "boolean") {
+    return arg1 === arg2;
+  }
+  if (typeof arg1 === "undefined" && typeof arg2 === "undefined") {
+    return arg1 === arg2;
+  }
+  if (typeof arg1 === "symbol" && typeof arg2 === "symbol") {
+    return arg1 === arg2;
+  }
+  if (Array.isArray(arg1) && Array.isArray(arg2)) {
+    if (arg1.length !== arg2.length) return false;
+
+    for (let i = 0; i < arg1.length; i++) {
+      if (!isEqual(arg1[i], arg2[i])) return false;
+    }
+
+    return true;
+  }
+
+  if (arg1 === null && arg2 === null) {
+    return arg1 === arg2;
+  }
+  if (typeof arg1 === "object" && typeof arg2 === "object") {
+    const key1 = Object.keys(arg1);
+    const key2 = Object.keys(arg2);
+
+    if (key1.length !== key2.length) {
+      return false;
+    }
+
+    for (let key of key1) {
+      if (!key2.includes(key)) {
+        return false;
+      }
+
+      if (!isEqual(arg1[key], arg2[key])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  if (typeof arg1 === "function" && typeof arg2 === "function") {
+    return arg1 === arg2;
+  }
+}
+const result = isEqual(
+  () => {
+    return 1;
+  },
+  () => {
+    return 1;
+  }
+);
+
+console.log(NaN === NaN);
+
+console.log(Object.is(NaN, NaN));
+console.log(result);
